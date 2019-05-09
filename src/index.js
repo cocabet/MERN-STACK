62d08d8a731d8d266b7842710cc08bc4e3d7aff7
Oscar <cocabet@gmail.com>
2019-05-09 11:31:27 -0500

    Se instaló el modulo morgarn, se agregó el redame.md

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
-app.listen(3000, () => {
-    console.log("Server in 3000");
+//Settings
+app.set('port', process.env.PORT || 3000);
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+//Routes
+
+//Static files
+
+//Starting server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
 });
